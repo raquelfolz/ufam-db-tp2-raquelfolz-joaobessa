fdf26511e87c9f99e52b20a570d0354c37ce4b66
Raquel Folz <raquel.folz@icomp.ufam.edu.br>
2023-05-21 02:34:33 -0400

    Improve comments and formatting

--- upload.cpp
+++ upload.cpp
@@ -10,19 +10,13 @@ struct Article {
   std::string snippet;
 };
 
-// Função auxiliar para dividir uma string em tokens com base em um delimitador
+// Função auxiliar para dividir a string em tokens correspondentes a cada campo
 std::vector<std::string> split(const std::string& input, char delimiter) {
   std::vector<std::string> tokens;
-  //std::stringstream ss(input);
-  std::string token;
-  /*while (std::getline(ss, token, delimiter)) {
-    if (token.size() < 3) tokens.push_back("");
-    else tokens.push_back(token.substr(1, token.size()-2));
-  }*/
-  token = "";
-  bool s = false; //true se ja passou uma aspa
+  std::string token = "";
+  bool ctrl = false; //true se esta atualmente lendo um token
   for (int i = 0; i < input.size(); i++) {
-    if (s) { //esta lendo um token
+    if (ctrl) {
       if (input[i] == '\"') {
         //aspas podem ou nao marcar o fim do token
         //se for "", entao eh o fim
@@ -31,13 +25,14 @@ std::vector<std::string> split(const std::string& input, char delimiter) {
           token += "\"\"";
           i++;
         }
-        else s = false;
+        else ctrl = false;
       }
-      else token += input[i]; //outros caracteres sempre fazem parte
+      else token += input[i]; //outros caracteres sempre fazem parte do token
       continue;
     }
-    //se nao esta lendo um token, pode ser inicio de outro ou separador ou NULL
-    if (input[i] == '\"') s = true;
+    //se nao esta lendo um token, pode ser inicio de outro token ou NULL ou ;
+    //outros casos nao foram tratados porque nao ocorrem no arquivo
+    if (input[i] == '\"') ctrl = true;
     else if (input[i] == ';'){
       tokens.push_back(token);
       token = "";
@@ -45,9 +40,8 @@ std::vector<std::string> split(const std::string& input, char delimiter) {
     else if (i < input.size()-3 and input.substr(i, 4) == "NULL") {
       i += 3;
     }
-    else throw std:: invalid_argument(input); //so para testar
   }
-  //depois do for, ainda tem que colocar o ultimo token ja que linha nao termina em ;
+  //depois do for, ainda tem que inserir o ultimo token ja que linha nao termina em ;
   tokens.push_back(token);
 
   return tokens;
@@ -60,7 +54,10 @@ void upload(const std::string& filename) {
     std::cout << "Erro ao abrir o arquivo: " << filename << std::endl;
     return;
   }
-
+  
+  //estruturas da biblioteca paddrao para tabela hash e arvore redblack
+  //idealmente devem ser trocados por estruturas proprias que permitam
+  //trabalhar com memoria secundaria, mas esses funcionam
   std::unordered_map<int, Article> dataHash;
   std::map<int, int> primaryIndex;
   std::map<std::string, int> secondaryIndex;
@@ -69,6 +66,8 @@ void upload(const std::string& filename) {
   std::string aux;
   while (std::getline(file, line)) {
     while (line.size() == 0 or line.back() != '\r') {
+      //tem alguns casos que acontece \n no meio de uma chave, entao
+      //so \r pode ser usado para efetivamente determinar o fim de uma entrada
       std::getline(file, aux);
       line += '\n';
       line += aux;
@@ -95,7 +94,8 @@ void upload(const std::string& filename) {
     }
   }
 
-  // Salvar os dados em arquivos (arquivo de dados organizado por hashing, arquivo de índice primário e arquivo de índice secundário)
+  // Salvar os dados em arquivos (arquivo de dados organizado por hashing,
+  // arquivo de índice primário e arquivo de índice secundário)
   std::ofstream dataFile("data.dat", std::ios::binary);
   for (const auto& pair : dataHash) {
     const Article& article = pair.second;
